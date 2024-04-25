c82919619a83e5b75575a6e69dbd149d4f6c5b14
Eike <eikedase@gmail.com>
2024-04-25 09:52:47 -0300

    Subindo Projeto Java

---
+++ Aluno.java
@@ -0,0 +1,16 @@
+package br.com.estacio.pessoa;
+
+public class Aluno extends Pessoa {
+
+	private String matricula;
+	
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	
+
+}
