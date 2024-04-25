2d0e30440aa370e09b7b5dfd6dece89462b4a8da
eike05 <163324004+eike05@users.noreply.github.com>
2024-04-25 10:22:09 -0300

    Add files via upload

---
+++ Coordenador.java
@@ -0,0 +1,18 @@
+package br.com.estacio.pessoa;
+
+public class Coordenador extends Funcionario {
+	private String curso;
+
+	public String getCurso() {
+		return curso;
+	}
+
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	
+	public void aumentarSalario(double percentual) {
+      
+    }
+	
+}
